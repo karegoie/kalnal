9d23267e5060ebef8bd5cfbc3487def8634f9c9c
karegoie <karegoie@snu.ac.kr>
2022-12-05 10:58:44 +0900

    1205 kalnal project finished

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10000)
+
 import os
 import subprocess
 import Bio.SeqIO as SeqIO
@@ -7,7 +10,7 @@ import itertools
 from scipy.cluster.hierarchy import dendrogram, linkage
 from matplotlib import pyplot as plt
 import copy
-import numpy as np
+import pandas as pd
 
 def ltr_finding(genome_path, threads):
     try:
@@ -15,19 +18,17 @@ def ltr_finding(genome_path, threads):
     except subprocess.CalledProcessError:
         print('Something went wrong......')
    
-   return 
-
 
 def pre_processing(genome_path):
-    genome_data = []
+    genome_data = {}
 
     with open(f'{genome_path}', 'r') as f:
         for record in SeqIO.parse(f, 'fasta'):
-            if not (bool(re.compile('.*(hlo|ito|onti).*').search(record.description)))
-                genome_data[record.id] = str(record.seq)
+            if not (bool(re.compile('.*(hlo|ito|onti).*').search(record.description))):
+                genome_data[f'{record.id}'] = str(record.seq)
 
-    if os.path.exists('./temp'):
-        os.makedir('./temp')
+    if not os.path.exists('./temp'):
+        os.mkdir('./temp')
 
     with open(f'temp/genome.fa', 'w') as g:
         for k, v in genome_data.items():
@@ -35,23 +36,25 @@ def pre_processing(genome_path):
 
     del genome_data
 
-    return
-
 
 def ltr_extract():
-    genome_data = []
+    genome_data = {}
     with open('temp/genome.fa', 'r') as f:
         for record in SeqIO.parse(f, 'fasta'):
-            genome_data[record.id] = record.seq
+            genome_data[f'{record.id}'] = record.seq
 
         extract = {}
-        with open('*.scn', 'r') as g:
+        file = [f for f in os.listdir() if f.endswith('.scn')][0]
+        with open(file, 'r') as g:
             for line in g:
-                chro = str(line.split("\t")[1].strip())
-                start = int(line.split("\t")[2].strip().split("-")[0]) - 1
-                end = int(line.split("\t")[2].strip().split("-")[1]) - 1
-                if chro not in extract.keys(): extract[chro] = ''
-                extract[chro] += genome_data[record.id][start:end+1]
+                try: 
+                    chro = str(line.split("\t")[1].strip())
+                    start = int(line.split("\t")[2].strip().split("-")[0]) - 1
+                    end = int(line.split("\t")[2].strip().split("-")[1]) - 1
+                    if chro not in extract.keys(): extract[chro] = ''
+                    extract[chro] += genome_data[record.id][start:end+1]
+                except ValueError:
+                    continue
 
         with open('temp/extract.fa', 'w') as h:
             for k, v in extract.items():
@@ -65,14 +68,17 @@ def kmer_count(kmer):
     split_command = '''awk -F "|" '/^>/ {close(F) ; F = "temp/"substr($1,2,length($1)-1)".split.fa"} {print >> F}' temp/extract.fa'''
     os.system(split_command)
     try:
-        for split_file in os.listdir('temp/*.split.fa'):
-            subprocess(['kalnal-kmer/target/release/kalnal', kmer, f'{split_file}', '>', 'temp/{split_file.split(".")[0].split("/")[1]}_kmer_count.tsv'], check = True)
+        file_list1 = [f for f in os.listdir('./temp') if f.endswith('.split.fa')]
+        for split_file in file_list1:
+            with open(f'temp/{split_file.split(".")[0]}_kmer_count.tsv', 'w') as f:
+                subprocess.run(['kalnal-kmer/target/release/kalnal-kmer', str(kmer), f'temp/{split_file}'], stdout=f, check = True)
     except subprocess.CalledProcessError:
         print("Something went wrong with kalnal-kmer")
 
     kmer_data = {}
-    for split_file in os,listdir('temp/*.tsv')
-        with open(split_file, 'r') as f:
+    file_list2 = [f for f in os.listdir('./temp') if f.endswith('.tsv')]
+    for split_file in file_list2:
+        with open(f'temp/{split_file}', 'r') as f:
             if split_file.split("_")[0] not in kmer_data.keys(): kmer_data[split_file.split("_")[0]] = {}
             for line in f:
                 kmer_data[split_file.split("_")[0]][line.split('\t')[0].strip()] = line.split('\t')[1].strip()
@@ -80,12 +86,13 @@ def kmer_count(kmer):
     with open(f'temp/K{kmer}.combine.json', 'w') as f:
         json.dump(kmer_data, f)
 
-    os.remove('temp/*.tsv')
-    os.remove('temp/*.split.fa')
+    for r in [f for f in os.listdir('./temp') if f.endswith('*.tsv')]:
+        os.remove(r)
+    for r in [f for f in os.listdir('./temp') if f.endswith('*.split.fa')]:
+        os.remove(r)
+
     del kmer_data
     
-    return
-
 
 def dict2ndarray(d, kmer):
     kmer_list = []
@@ -101,42 +108,47 @@ def dict2ndarray(d, kmer):
         real_data[k1] = sorted(v1.items())
 
     del d
-
-    return np.fromiter(sorted(real_data.items()), dtype=object, count=len(real_data))
+    
+    final_data = {}
+    for k, v in real_data.items():
+        for mer, n in v:
+            if k not in final_data.keys(): final_data[k] = []
+            final_data[k].append(n)
+   
+    return final_data
 
 
 def ploting(kmer):
-    data = dict(json.load(f'temp/K{kmer}.combine.json'))
+    with open(f'temp/K{kmer}.combine.json') as f:
+        data = dict(json.load(f))
 
     plt.figure(figsize=(30, 20))
     data = dict2ndarray(data, kmer)
-    linked = linkage(data, method='ward')
-    dend = dendrogram(linked, orientation='top', distance_sort='descending', labels=list(df.T.index), show_leaf_counts=True)
+    data = pd.DataFrame(data)
+    linked = linkage(data.T, method='ward')
+    dend = dendrogram(linked, orientation='top', distance_sort='descending', labels=list(data.T.index), show_leaf_counts=True)
 
     plt.savefig(f'{kmer}_analyzed.png')
 
 
 def finalize():
-    if os.path.exists('*.scn'):
-        os.remove('*.scn')
-        os.remove('*.gff3')
-        os.remove('*.list')
+    if  len([f for f in os.listdir() if f.endswith('.scn')]):
+        os.remove([f for f in os.listdir() if f.endswith('.scn')][0])
+        os.remove([f for f in os.listdir() if f.endswith('.gff3')][0])
+        os.remove([f for f in os.listdir() if f.endswith('.list')][0])
 
     if os.path.exists('./temp'):
-        os.remove('temp/*')
+        for r in os.listdir('./temp'):
+            os.remove(f'temp/{r}')
         os.rmdir('./temp')
 
 
 def analyze(args):
-    pre_processing(args.genome)
-    ltr_finding(args.genome, args.threads)
-    ltr_extract()
-    kmer = int(args.kmer)
-    for k in [kmer-4, kmer-2, kmer, kmer+2, kmer+4]:
-        kmer_count(k)
-        ploting(k)
+#    pre_processing(args.genome)
+#    ltr_finding(args.genome, args.threads)
+#    ltr_extract()
+#    kmer = int(args.kmer)
+#    for k in [kmer-4, kmer-2, kmer]:
+#        kmer_count(k)
+#        ploting(k)
     finalize()
-
-
-
-
--- kalnal.py
+++ kalnal.py
@@ -4,10 +4,10 @@ import argparse
 from functions import analyze
 
 def parse():
-    parser = argparse.ArgumentParser(description="GUM; the polyploid subgenome divider", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-g', '--genome', type=int, default=None, help='Scaffolded target genome file')
-    parser.add_argument('-k', '--kmer', type=int, default=21, help='parameter for k-mer analysis, (k-4), (k-2), k, (k+2), (k+4) will be analyzed')
-    parser.add_argument('-t', '--threads', type=int, defaulg=12, help='number of threads for calculation')
+    parser = argparse.ArgumentParser(description="Kalnal - The polyploid subgenome divider", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-g', '--genome', type=str, default=None, help='Scaffolded target genome file')
+    parser.add_argument('-k', '--kmer', type=int, default=21, help='parameter for k-mer analysis, (k-4), (k-2), k will be analyzed')
+    parser.add_argument('-t', '--threads', type=int, default=12, help='number of threads for calculation')
 
     args = parser.parse_args()
 
